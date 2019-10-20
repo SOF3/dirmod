cf60ae71ca62db4407c1f2f2298cec9b4f0b6440
Travis-CI: cargo doc <sofe2038@gmail.com>
2019-10-20 05:57:00 +0000

    Travis-CI doc build: Fixed directory cleanness problems in travis/deploy.sh

---
+++ latest/aliases.js
@@ -0,0 +1,8 @@
+var ALIASES = {};
+ALIASES["dirmod"] = {};
+ALIASES["doc_comment"] = {};
+ALIASES["proc_macro2"] = {};
+ALIASES["quote"] = {};
+ALIASES["smallvec"] = {};
+ALIASES["syn"] = {};
+ALIASES["unicode_xid"] = {};
