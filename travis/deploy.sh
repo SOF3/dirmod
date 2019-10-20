2ff6a0ae42d664d4b68958bb9712beebf710f872
SOFe <sofe2038@gmail.com>
2019-10-20 13:47:04 +0800

    gh-pages: Update index.html with latest tag

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == master ]] && [[ $RELASE_FLAG == "--release" ]]; then
+if [[ $TRAVIS_BRANCH == master ]] && [[ $RELEASE_FLAG == "--release" ]]; then
   git clone https://${GH_TOKEN}@github.com/SOF3/dirmod.git --branch=gh-pages $HOME/gh-pages
   cp -r target/doc/* $HOME/gh-pages/latest
 	if [[ $TRAVIS_TAG ]]; then
 		cp -r target/doc/ $HOME/gh-pages/$TRAVIS_TAG
+		echo "<html><head><meta http-equiv='refresh' content='0; url=./${TRAVIS_TAG}/dirmod/'/></head><body>Redirecting...</body></html>" > $HOME/gh-pages/index.html
 	fi
   cd $HOME/gh-pages
   git config user.name "Travis-CI: cargo doc"
@@ -12,4 +13,6 @@ if [[ $TRAVIS_BRANCH == master ]] && [[ $RELASE_FLAG == "--release" ]]; then
   git add .
   git commit -m "Travis-CI doc build: $TRAVIS_COMMIT_MESSAGE"
   git push -u origin gh-pages
+else
+	echo "Only executing deployment on master with --release"
 fi
