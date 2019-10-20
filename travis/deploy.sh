42cdc81999f606d8cb13a0193f1deb9b38b27ae5
SOFe <sofe2038@gmail.com>
2019-10-20 13:53:37 +0800

    Fixed directory cleanness problems in travis/deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,8 +2,10 @@
 
 if [[ $TRAVIS_BRANCH == master ]] && [[ $RELEASE_FLAG == "--release" ]]; then
   git clone https://${GH_TOKEN}@github.com/SOF3/dirmod.git --branch=gh-pages $HOME/gh-pages
-  cp -r target/doc/* $HOME/gh-pages/latest
+	[[ -d $HOME/gh-pages/latest ]] && rm -r $HOME/gh-pages/latest
+  cp -r target/doc/ $HOME/gh-pages/latest
 	if [[ $TRAVIS_TAG ]]; then
+		[[ -d $HOME/gh-pages/$TRAVIS_TAG ]] && rm -r $HOME/gh-pages/$TRAVIS_TAG
 		cp -r target/doc/ $HOME/gh-pages/$TRAVIS_TAG
 		echo "<html><head><meta http-equiv='refresh' content='0; url=./${TRAVIS_TAG}/dirmod/'/></head><body>Redirecting...</body></html>" > $HOME/gh-pages/index.html
 	fi
