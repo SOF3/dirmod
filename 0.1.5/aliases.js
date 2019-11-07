b235fcd35812b4dbc3f45c6c452d168d82be3967
Travis-CI: cargo doc <sofe2038@gmail.com>
2019-11-07 03:59:28 +0000

    Travis-CI doc: 0.1.5: 0.1.5 update

---
+++ 0.1.5/aliases.js
@@ -0,0 +1,10 @@
+var ALIASES = {};
+ALIASES["dirmod"] = {};
+ALIASES["dirmod_codegen"] = {};
+ALIASES["doc_comment"] = {};
+ALIASES["maybe_uninit"] = {};
+ALIASES["proc_macro2"] = {};
+ALIASES["quote"] = {};
+ALIASES["smallvec"] = {};
+ALIASES["syn"] = {};
+ALIASES["unicode_xid"] = {};
---
+++ 0.1.5/source-files.js
@@ -0,0 +1,11 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["dirmod"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["dirmod_codegen"] = {"name":"","dirs":[{"name":"parse","files":["all.rs","arg.rs","cfg.rs","mod.rs","modifier.rs","priv_vis.rs"]}],"files":["imp.rs","lib.rs"]};
+sourcesIndex["doc_comment"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["maybe_uninit"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["proc_macro2"] = {"name":"","files":["fallback.rs","lib.rs","strnom.rs","wrapper.rs"]};
+sourcesIndex["quote"] = {"name":"","files":["ext.rs","format.rs","ident_fragment.rs","lib.rs","runtime.rs","spanned.rs","to_tokens.rs"]};
+sourcesIndex["smallvec"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["syn"] = {"name":"","dirs":[{"name":"gen","files":["gen_helper.rs"]}],"files":["attr.rs","bigint.rs","buffer.rs","custom_keyword.rs","custom_punctuation.rs","data.rs","derive.rs","discouraged.rs","error.rs","export.rs","expr.rs","ext.rs","generics.rs","group.rs","ident.rs","lib.rs","lifetime.rs","lit.rs","lookahead.rs","mac.rs","macros.rs","op.rs","parse.rs","parse_macro_input.rs","parse_quote.rs","path.rs","print.rs","punctuated.rs","sealed.rs","span.rs","spanned.rs","thread.rs","token.rs","tt.rs","ty.rs"]};
+sourcesIndex["unicode_xid"] = {"name":"","files":["lib.rs","tables.rs"]};
+createSourceSidebar();
